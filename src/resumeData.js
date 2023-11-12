1f615fcf813ed37fc59a626158b4249169bda0fa
Passakorn11 <110074241+Passakorn11@users.noreply.github.com>
2023-11-12 23:57:11 +0700

    portfolio
    portfolio

---
+++ src/resumeData.js
@@ -0,0 +1,56 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Passakorn Sinpeng",
+    "role": "Frontend Developer",
+    "E-mail":"mailto:1464464@gmail.com",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I am a student .I like dabbling in various parts of frontend development and like to learn about new technologies  and play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"E-mail",
+          "url":"mailto:1464464@gmail.com",
+          "className":"fa fa-envelope"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Passakorn11",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"I study at King Mongkut's University of Technology North Bangkok. I am in my 4th year in the field of Electronic Engineering Technology. and will be a Frontend Developer",
+    "address":"Thailand",
+    "website":"https://github.com/Passakorn11",
+    "education":[
+      {
+        "UniversityName":"King Mongkut's University of Technology North Bangkok",
+        "specialization":"When entered to study",
+        "MonthOfPassing":"Jun",
+        "YearOfPassing":"2020",
+      },
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    
+    "testimonials":[
+      {
+        "description":"This is a my profile",
+        "name":"I am Passakorn Sinpeng"
+      },
+      {
+        "description":"university",
+        "name":"King Mongkut's University of Technology North Bangkok."
+      }
+    ]
+  }
+  
+  export default resumeData
